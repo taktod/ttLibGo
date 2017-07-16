d34682fa4fad61052c092bc5746bf664f2772b58
taktod <poepoemix@hotmail.com>
2017-07-16 13:27:07 +0900

    libyuvによる動作も一応かいてみた。これはテストしてない。

---
+++ ttLibGoLibyuv/doc.go
@@ -0,0 +1,11 @@
+/*
+Package ttLibGoLibyuv
+libyuvのライブラリを利用した動作
+
+ライセンスは3-Clause BSDとしておきます
+linuxだとcmakeでコンパイルしてinstallすると/usr/local/libと/usr/local/includeにインストールされました。
+OSXだとmakeでコンパイルしたあとに自分でライブラリを配置して利用してますね。
+まぁ、ffmpegのswscaleで事足りますけど
+*/
+package ttLibGoLibyuv
+
---
+++ ttLibGoLibyuv/source.go
@@ -0,0 +1,8 @@
+package ttLibGoLibyuv
+
+/*
+#cgo CFLAGS: -I../ -I../ttLibC/ -I/usr/local/include -std=c99 -D__ENABLE_LIBYUV__=1
+#cgo LDFLAGS: -lc++ -L/usr/local/lib -lyuv
+#cgo CXXFLAGS: -std=c++0x -I../ -I../ttLibC/ -I/usr/local/include -D__ENABLE_LIBYUV__=1
+*/
+import "C"
