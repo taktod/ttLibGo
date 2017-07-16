11e05570c6fae7b51839b649b48035d1d5ba7e41
taktod <poepoemix@hotmail.com>
2017-07-16 17:29:13 +0900

    x265書いた、とりあえず欲しいのはこれで終わり

---
+++ ttLibGoX265/doc.go
@@ -0,0 +1,8 @@
+/*
+Package ttLibGoX265
+x265のライブラリを利用したコード
+
+x265を利用してh265を作るプログラム
+*/
+package ttLibGoX265
+
---
+++ ttLibGoX265/source.go
@@ -0,0 +1,9 @@
+package ttLibGoX265
+
+/*
+#cgo pkg-config: x265
+#cgo CFLAGS: -I../ -I../ttLibC/ -std=c99 -D__ENABLE_X265__=1
+#cgo LDFLAGS: -lc++
+#cgo CXXFLAGS: -std=c++0x -I../ -I../ttLibC/ -D__ENABLE_X265__=1
+*/
+import "C"
