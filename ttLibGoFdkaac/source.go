12ee8bb175b14ad516d8594e0e9d026c47cae5e2
taktod <poepoemix@hotmail.com>
2017-07-23 09:55:02 +0900

    defが間違っていたので変更した。

--- ttLibGoFdkaac/source.go
+++ ttLibGoFdkaac/source.go
@@ -2,8 +2,8 @@ package ttLibGoFdkaac
 
 /*
 #cgo pkg-config: fdk-aac
-#cgo CFLAGS: -I../ -I../ttLibC/ -I/usr/local/include -std=c99 -D__ENABLE_FAAC_ENCODE__=1
+#cgo CFLAGS: -I../ -I../ttLibC/ -I/usr/local/include -std=c99 -D__ENABLE_FDKAAC_ENCODE__=1
 #cgo LDFLAGS: -lc++ -L/usr/local/lib
-#cgo CXXFLAGS: -std=c++0x -I../ -I../ttLibC/ -I/usr/local/include -D__ENABLE_FAAC_ENCODE__=1
+#cgo CXXFLAGS: -std=c++0x -I../ -I../ttLibC/ -I/usr/local/include -D__ENABLE_FDKAAC_ENCODE__=1
 */
 import "C"
